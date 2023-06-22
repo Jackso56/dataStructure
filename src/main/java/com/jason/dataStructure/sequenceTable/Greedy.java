da61fed17ce48d39f32b64a9136d3fe4de05db3c
jason <2574550346@qq.com>
2023-06-22 10:38:34 +0800

    202306221038

--- src/main/java/com/jason/dataStructure/sequenceTable/Greedy.java
+++ src/main/java/com/jason/dataStructure/sequenceTable/Greedy.java
@@ -1,4 +1,4 @@
-package com.jason.dataStructure.Greedy;
+package com.jason.dataStructure.sequenceTable;
 
 import java.util.*;
 
@@ -8,7 +8,7 @@ import java.util.*;
  * @CreateTime 2023/6/18 09:26:31
  **/
 @SuppressWarnings({"all"})
-public class demo01 {
+public class Greedy {
 
     /**
      * 贪心算法概述：
@@ -631,5 +631,168 @@ public class demo01 {
         return sum;
     }
 
+
+    /**
+     * 数组拆分
+     * <p>
+     * 给定长度为 2n 的整数数组 nums ，你的任务是将这些数分成 n 对,
+     * 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从 1 到 n 的 min(ai, bi) 总和最大。
+     * <p>
+     * 返回该 最大总和 。
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode.cn/leetbook/read/greedy/rv32p2/
+     * 来源：力扣（LeetCode）
+     *
+     * @param nums
+     * @return
+     */
+    public static int arrayPairSum(int[] nums) {
+        int len = nums.length >> 1, max = 0, min = 0, j = nums.length - 1, sum = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < len; i++) {
+            max = nums[j];
+            min = nums[j - 1];
+            j -= 2;
+            sum += min;
+        }
+        return sum;
+    }
+
+
+    /**
+     * 卡车上的最大单元数
+     * <p>
+     * 请你将一些箱子装在 一辆卡车 上。给你一个二维数组 boxTypes ，其中 boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi] ：
+     * <p>
+     * numberOfBoxesi 是类型 i 的箱子的数量。
+     * numberOfUnitsPerBoxi 是类型 i 每个箱子可以装载的单元数量。
+     * 整数 truckSize 表示卡车上可以装载 箱子 的 最大数量 。只要箱子数量不超过 truckSize ，你就可以选择任意箱子装到卡车上。
+     * <p>
+     * 返回卡车可以装载 单元 的 最大 总数。
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode.cn/leetbook/read/greedy/rvqpoc/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param boxTypes
+     * @param truckSize
+     * @return
+     */
+    public static int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes, (o1, o2) -> o2[1] - o1[1]);
+        // boxTypes[i][0] <= truckSize
+        int count = 0, units = 0;
+        int i = 0;
+        while (count < truckSize && i < boxTypes.length) {
+            if (count + boxTypes[i][0] <= truckSize) {
+                units += boxTypes[i][1] * boxTypes[i][0];
+                count += boxTypes[i][0];
+                i++;
+            } else {
+                units += (truckSize - count) * boxTypes[i][1];
+                break;
+            }
+        }
+        return units;
+    }
+
+
+    /**
+     * 玩筹码
+     * 有 n 个筹码。第 i 个筹码的位置是 position[i] 。
+     * <p>
+     * 我们需要把所有筹码移到同一个位置。在一步中，我们可以将第 i 个筹码的位置从 position[i] 改变为:
+     * <p>
+     * position[i] + 2 或 position[i] - 2 ，此时 cost = 0
+     * position[i] + 1 或 position[i] - 1 ，此时 cost = 1
+     * 返回将所有筹码移动到同一位置上所需要的 最小代价 。
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode.cn/leetbook/read/greedy/rv6eei/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param position
+     * @return
+     */
+    public static int minCostToMoveChips(int[] position) {
+        int odd = 0, even = 0;
+        for (int i = 0; i < position.length; i++) {
+            // 基数之间和偶数之间无消耗，奇偶之间消耗为1
+            if (position[i] % 2 == 0) {
+                odd++;
+            } else {
+                even++;
+            }
+        }
+        return odd > even ? even : odd;
+    }
+
+
+    /**
+     * 交换字符使得字符串相同
+     * <p>
+     * 有两个长度相同的字符串 s1 和 s2，且它们其中 只含有 字符 "x" 和 "y"，你需要通过「交换字符」的方式使这两个字符串相同。
+     * <p>
+     * 每次「交换字符」的时候，你都可以在两个字符串中各选一个字符进行交换。
+     * <p>
+     * 交换只能发生在两个不同的字符串之间，绝对不能发生在同一个字符串内部。也就是说，我们可以交换 s1[i] 和 s2[j]，但不能交换 s1[i] 和 s1[j]。
+     * <p>
+     * 最后，请你返回使 s1 和 s2 相同的最小交换次数，如果没有方法能够使得这两个字符串相同，则返回 -1 。
+     * <p>
+     *  
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode.cn/leetbook/read/greedy/rvkx9e/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public int minimumSwap(String s1, String s2) {
+        // 核心：x y 不同的个数
+        char[] char1 = s1.toCharArray();
+        char[] char2 = s2.toCharArray();
+        int xHave = 0;
+        int yHave = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (char1[i] - char2[i] < 0) {
+                xHave++;
+            } else if (char1[i] - char2[i] > 0) {
+                yHave++;
+            }
+        }
+        if ((xHave + yHave) % 2 != 0) {
+            return -1;
+        }
+        // 多数的一次交换 +一个两次交换
+        // (xHave + yHave) / 2 的方式输出答案，会直接排出基数的情况
+        return xHave / 2 + yHave / 2 + xHave % 2 * 2;
+    }
+
+
+    /**
+     * 构造 K 个回文字符串
+     *
+     * 给你一个字符串 s 和一个整数 k 。请你用 s 字符串中 所有字符 构造 k 个非空 回文串 。
+     *
+     * 如果你可以用 s 中所有字符构造 k 个回文字符串，那么请你返回 True ，否则返回 False 。
+     *
+     * 作者：LeetCode
+     * 链接：https://leetcode.cn/leetbook/read/greedy/rvkjxg/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param s
+     * @param k
+     * @return
+     */
+    public boolean canConstruct(String s,int k){
+
+        return false;
+    }
 
 }
