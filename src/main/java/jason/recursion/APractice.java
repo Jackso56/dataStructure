b7f818fe2a3ee22335211b0841204324c7a08add
jason <2574550346@qq.com>
2023-06-25 16:23:40 +0800

    202306251623

---
+++ src/main/java/jason/recursion/APractice.java
@@ -0,0 +1,101 @@
+package jason.recursion;
+
+/**
+ * @author Jason Black
+ * @version The past cannot be redeemed, the future can be changed.
+ * @CreateTime 2023/6/25 08:10:13
+ **/
+@SuppressWarnings({"all"})
+public class APractice {
+
+    public static void main(String[] args) {
+        System.out.println((1 ^ 2) + ((1 & 2) << 1));
+    }
+
+
+    /**
+     * Pow(x, n)
+     * <p>
+     * 核心：快速幂 + 递归
+     *
+     * <p>
+     * 实现 pow(x, n) ，即计算 x 的整数 n 次幂函数（即，xn ）。
+     *
+     * @param x
+     * @param n
+     * @return
+     */
+    public static double myPow(double x, int n) {
+        // 此处需要使用 long 数据类型  double 可能会溢出
+        long count = n;
+        return n >= 0 ? quickPow(x, count) : 1.0 / quickPow(x, -count);
+    }
+
+    private static double quickPow(double x, long count) {
+        if (count == 0) {
+            return 1.0;
+        }
+        System.out.println(count / 2);
+        double result = quickPow(x, count / 2);
+        System.out.println(result);
+        return count % 2 == 0 ? result * result : result * result * x;
+    }
+
+
+    /**
+     * 不用加减乘除做加法
+     * <p>
+     * 基础知识：
+     * 1. a^b  得到无进位和
+     * 2. (a&b)<<1  得到进位
+     * 3. (a^b) + ((a&b)<<1)  即为最终答案
+     * <p>
+     * <p>
+     * 写一个函数，求两个整数之和，要求在函数体内不得使用 “+”、“-”、“*”、“/” 四则运算符号。
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int add(int a, int b) {
+        // 边界条件
+        if (b == 0) {
+            return a;
+        }
+        // a ^ 0 == a
+        return add(a ^ b, (a & b) << 1);
+    }
+
+
+    /**
+     * 递归乘法
+     * <p></>
+     * 核心：第 0为 1 时 --> answer = max << 0; 第 i为 1 时 --> answer = max << i
+     *
+     * <p>
+     * 递归乘法。 写一个递归函数，不使用 * 运算符， 实现两个正整数的相乘。可以使用加号、减号、位移，但要吝啬一些。
+     * <p></>
+     *
+     * @param A
+     * @param B
+     * @return
+     */
+    public static int multiply(int A, int B) {
+        int max = A > B ? A : B;
+        int min = max == A ? B : A;
+        return recursionMultiply(min, max, 0);
+    }
+
+    private static int recursionMultiply(int min, int max, int index) {
+        int answer = 0;
+        if (min >> index == 0) {
+            return 0;
+        }
+        if (((min >> index) & 1) == 1) {
+            answer = max << index;
+        }
+        return answer + recursionMultiply(min, max, index++);
+    }
+
+
+}
---
+++ src/main/java/jason/recursion/BInDepthUnderstandingRecursion.java
@@ -0,0 +1,76 @@
+package main.java.jason.recursion;
+
+import java.util.Arrays;
+
+import static com.sun.webkit.perf.WCGraphicsPerfLogger.log;
+
+
+/**
+ * @author Jason Black
+ * @version The past cannot be redeemed, the future can be changed.
+ * @CreateTime 2023/6/25 14:27:04
+ **/
+
+@SuppressWarnings({"all"})
+public class BInDepthUnderstandingRecursion {
+
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(mergeSort(new int[]{8, 7, 4, 1, 8, 3, 2, 7, 4, 9, 3, 2, 4, 9})));
+    }
+
+    /**
+     * 归并排序 --> 分治，递归的经典案例
+     * <p></>
+     * partition() --> 实现分治  &nbsp;&nbsp; merge() --> 实现了归并
+     *
+     * @param nums
+     * @return
+     */
+    public static int[] mergeSort(int[] nums) {
+        int len = nums.length;
+        int[] tempNums = new int[len];
+        partition(nums, 0, len - 1, tempNums);
+        return nums;
+    }
+
+    private static void partition(int[] nums, int left, int right, int[] tempNums) {
+        System.out.println("X拆分子问题" + "[" + left + "," + right + "]");
+        // 边界条件
+        if (left == right) {
+            System.out.println("Y解决子问题" + "[" + left + "," + right + "]");
+            return;
+        }
+        int middle = (left + right) >> 1;
+        // 开始分治  -->  大问题拆分成一个个胡小问题
+        partition(nums, left, middle, tempNums);
+        partition(nums, middle + 1, right, tempNums);
+        // 开始归并  -->  递归求解个个小问题
+        merge(nums, left, middle, right, tempNums);
+        System.out.println("Y解决子问题" + "[" + left + "," + right + "]");
+    }
+
+    private static void merge(int[] nums, int left, int middle, int right, int[] tempNums) {
+        // 复制数组(方便下面的操作)
+        for (int i = left; i <= right; i++) {
+            tempNums[i] = nums[i];
+        }
+        // index for nums;   tempLeft, tempRight for tempNums
+        int index = left, tempLeft = left, tempRight = middle + 1;
+        // 未超出边界的情况下，把较小的数放在数组的前面（数值来自tempNums），其中 tempLeft <= middle 可以保持数组的稳定性
+        while (tempLeft <= middle && tempRight <= right) {
+            if (tempNums[tempLeft] <= tempNums[tempRight]) {
+                nums[index++] = tempNums[tempLeft++];
+            } else {
+                nums[index++] = tempNums[tempRight++];
+            }
+        }
+        // 当有一边已超出边界，另一边直接将数值直接复制到原数组中即可
+        while (tempLeft <= middle) {
+            nums[index++] = tempNums[tempLeft++];
+        }
+        while (tempRight <= right) {
+            nums[index++] = tempNums[tempRight++];
+        }
+    }
+}
